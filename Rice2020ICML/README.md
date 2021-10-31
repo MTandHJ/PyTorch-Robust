288f160e05e69677c1e9df616db852de6de3de1e
MTandHJ <congxueric@gmail.com>
2021-10-31 13:32:15 +0800

    add Rice2020ICML

---
+++ Rice2020ICML/README.md
@@ -0,0 +1,24 @@
+
+
+
+## Rice2020ICML
+
+
+> [Rice L., Wong E. \& Kolter J. Z. Overfitting in adversarially robust deep learning. In International Conference on Machine Learning (ICML), 2020.](http://arxiv.org/abs/2002.11569)
+
+[official-code](https://github.com/locuslab/robust_overfitting)
+
+
+
+### CIFAR-10
+
+    python AT.py resnet18 cifar10 -lp=Rice2020ICML --epochs=200 -wd=5e-4 --ratio=0.02
+
+### CIFAR-100
+
+
+    python AT.py resnet18 cifar100 -lp=Rice2020ICML --epochs=200 -wd=5e-4 --ratio=0.02
+
+
+
+## Evaluation
---
+++ Rice2020ICML/src/loss_zoo.py
@@ -0,0 +1,48 @@
+
+
+from typing import Optional, Union, Iterable
+import torch
+import torch.nn.functional as F
+
+
+
+
+def cross_entropy(
+    outs: torch.Tensor, 
+    labels: torch.Tensor, 
+    reduction: str = "mean"
+) -> torch.Tensor:
+    """
+    cross entropy with logits
+    """
+    return F.cross_entropy(outs, labels, reduction=reduction)
+
+def cross_entropy_softmax(
+    probs: torch.Tensor,
+    labels: torch.Tensor,
+    reduction: str = "mean"
+) -> torch.Tensor:
+    """
+    cross entropy with probs
+        probs: the softmax of logits
+    """
+    return F.nll_loss(probs.log(), labels, reduction=reduction)
+
+def kl_divergence(
+    logits: torch.Tensor, 
+    targets: torch.Tensor, 
+    reduction: str = "batchmean"
+) -> torch.Tensor:
+    # KL divergence
+    assert logits.size() == targets.size()
+    # targets = targets.clone().detach()
+    inputs = F.log_softmax(logits, dim=-1)
+    targets = F.softmax(targets, dim=-1)
+    return F.kl_div(inputs, targets, reduction=reduction)
+
+def lploss(
+    x: torch.Tensor,
+    p: Union[int, float, 'fro', 'nuc'] = 'fro',
+    dim: Union[int, Iterable] = -1
+):
+    return torch.norm(x, p=p, dim=dim).mean()
